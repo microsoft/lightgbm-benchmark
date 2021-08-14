e1c27546460ac7e0295608229a773773f5a6e24c
Jeff Omhover <jfomhover@gmail.com>
2021-08-14 15:34:05 -0700

    fix unit tests and format

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,9 @@ import os
 import sys
 import logging
 
-LIGHTGBM_BENCHMARK_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+LIGHTGBM_BENCHMARK_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "src")
+)
 
 if LIGHTGBM_BENCHMARK_ROOT not in sys.path:
     logging.info(f"Adding {LIGHTGBM_BENCHMARK_ROOT} to path")
--- tests/scripts/test_generate_data.py
+++ tests/scripts/test_generate_data.py
@@ -1,10 +1,11 @@
+"""Tests src/scripts/generate_data/generate.py"""
 import os
 import sys
 import tempfile
 import pytest
 from unittest.mock import patch
-
 from scripts.generate_data import generate
+
 
 @pytest.fixture()
 def temporary_dir():
@@ -13,21 +14,30 @@ def temporary_dir():
     yield temp_directory.name
     temp_directory.cleanup()
 
+
 @pytest.mark.parametrize("task_type", ["regression", "classification"])
 def test_generate_data(temporary_dir, task_type):
-    """ Tests src/scripts/generate_data/generate.py """
+    """Tests src/scripts/generate_data/generate.py"""
     output_dir = os.path.join(temporary_dir, "output")
 
     script_args = [
         "generate.py",
-        "--train_samples", "100",
-        "--test_samples", "10",
-        "--inferencing_samples", "100",
-        "--n_features", "40",
-        "--n_informative", "10",
-        "--random_state", "5",
-        "--output", output_dir,
-        "--type", "regression"
+        "--train_samples",
+        "100",
+        "--test_samples",
+        "10",
+        "--inferencing_samples",
+        "100",
+        "--n_features",
+        "40",
+        "--n_informative",
+        "10",
+        "--random_state",
+        "5",
+        "--output",
+        output_dir,
+        "--type",
+        "regression",
     ]
     if task_type == "classification":
         script_args.extend(["--n_redundant", "5"])
@@ -36,15 +46,12 @@ def test_generate_data(temporary_dir, task_type):
     with patch.object(sys, "argv", script_args):
         generate.main()
 
-    assert (
-        os.path.isfile(os.path.join(output_dir, "inference.txt")),
-        "Script generate.py should generate inference.txt under --output dir but did not"
-    )
-    assert (
-        os.path.isfile(os.path.join(output_dir, "train.txt")),
-        "Script generate.py should generate train.txt under --output dir but did not"
-    )
-    assert (
-        os.path.isfile(os.path.join(output_dir, "test.txt")),
-        "Script generate.py should generate test.txt under --output dir but did not"
-    )
+    assert os.path.isfile(
+        os.path.join(output_dir, "inference.txt")
+    ), "Script generate.py should generate inference.txt under --output dir but did not"
+    assert os.path.isfile(
+        os.path.join(output_dir, "train.txt")
+    ), "Script generate.py should generate train.txt under --output dir but did not"
+    assert os.path.isfile(
+        os.path.join(output_dir, "test.txt")
+    ), "Script generate.py should generate test.txt under --output dir but did not"
