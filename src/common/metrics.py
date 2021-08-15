90494fb7f4e93268dbcf8e76147110003b3337a1
Jeff Omhover <jfomhover@gmail.com>
2021-08-15 14:23:29 -0700

    mlflow test implementation

--- src/common/metrics.py
+++ src/common/metrics.py
@@ -7,6 +7,43 @@ These classes provide some tools to automate wall time compute and logging
 import os
 import time
 from functools import wraps
+import mlflow
+
+
+class MetricsLogger():
+    """
+    Class for handling metrics logging in a singleton
+
+    Example:
+    --------
+    >> from common.metrics import MetricsLogger
+    >>
+    >> metrics_logger = MetricsLogger()
+    >> metrics_logger.log_metrics("rmse", 0.456)
+    """
+    _initialized = False
+    _instance = None
+
+    def __new__(cls):
+        """ Create a new instance of the Singleton if necessary """
+        if cls._instance is None:
+            # if this is the first time we're initializing
+            cls._instance = super(MetricsLogger, cls).__new__(cls)
+            print("Initializing MLFLOW")
+            mlflow.start_run()
+        else:
+            # if this is not the first time
+            pass
+
+        return cls._instance
+
+    def log_metric(self, key, value):
+        print(f"mlflow.log_metric({key},{value})")
+        # NOTE: there's a limit to the name of a metric
+        if len(key) > 45:
+            key = key[:45]
+        mlflow.log_metric(key, value)
+
 
 ########################
 ### CODE BLOCK TIMER ###
@@ -54,6 +91,7 @@ class LogTimeBlock(object):
         # internal variables
         self.name = name
         self.start_time = None
+        self.metrics_logger = MetricsLogger()
 
     def __enter__(self):
         """ Starts the timer, gets triggered at beginning of code block """
@@ -69,6 +107,7 @@ class LogTimeBlock(object):
             if method == "print":
                 # just prints nicely
                 print(f"--- time elapsed: {self.name} = {run_time:2f} s" + (f" [tags: {self.tags}]" if self.tags else ""))
+                self.metrics_logger.log_metric(self.name, run_time)
             else:
                 # Place holder for mlflow
                 raise NotImplementedError("Nothing else exists at this point")
@@ -83,12 +122,13 @@ def log_time_function(func):
     """ decorator to log wall time of a function/method """
     @wraps(func)
     def perf_wrapper(*args, **kwargs):
-        log_name = "{}.time".format(func.__qualname__[:45]) # NOTE: there's a limit to the name of a metric
+        log_name = "{}.time".format(func.__qualname__)
         start_time = time.time()
         output = func(*args, **kwargs)
         run_time = time.time() - start_time
 
         print("--- time elapsed: {} = {:2f} s".format(log_name, run_time))
+        self.metrics_logger.log_metric(log_name, run_time)
 
         return output
     return perf_wrapper
