e334b067eccfd53adc251432d9959b12ace254c4
Jeff Omhover <jf.omhover@gmail.com>
2021-09-15 00:45:22 -0700

    remove prefix from callbacks

--- src/common/lightgbm.py
+++ src/common/lightgbm.py
@@ -9,15 +9,14 @@ import logging
 
 class LightGBMCallbackHandler():
     """ This class handles LightGBM callbacks for recording metrics. """
-    def __init__(self, metrics_logger, metrics_prefix=None):
+    def __init__(self, metrics_logger):
         """
         Args:
             metrics_logger (common.metrics.MetricsLogger)
-            metrics_prefix (str) : provide a prefix for all metrics (ex: node number)
+            node_index (int) : if mpi, provide index of the node
         """
         self.metrics = {}
         self.metrics_logger = metrics_logger
-        self.metrics_prefix = metrics_prefix
         self.logger = logging.getLogger(__name__)
     
     def callback(self, env: lightgbm.callback.CallbackEnv) -> None:
@@ -29,13 +28,9 @@ class LightGBMCallbackHandler():
 
         # loop on all the evaluation results tuples
         for data_name, eval_name, result, _ in env.evaluation_result_list:
-            metric_key = f"{data_name}.{eval_name}"
-            if self.metrics_prefix:
-                metric_key = self.metrics_prefix + metric_key
-
             # log each as a distinct metric
             self.metrics_logger.log_metric(
-                key=metric_key,
+                key=f"{data_name}.{eval_name}",
                 value=result,
                 step=env.iteration # provide iteration as step in mlflow
             )
