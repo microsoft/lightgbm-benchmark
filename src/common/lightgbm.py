14a16d44e0fbe31907e1854c3eb950e1d6454ac7
Jeff Omhover <jf.omhover@gmail.com>
2021-09-18 22:22:01 -0700

    send and receive metrics at end of callbacks

--- src/common/lightgbm.py
+++ src/common/lightgbm.py
@@ -6,6 +6,7 @@ This classes provide help to integrate lightgbm
 """
 import lightgbm
 import logging
+from mpi4py import MPI
 
 class LightGBMCallbackHandler():
     """ This class handles LightGBM callbacks for recording metrics. """
@@ -15,7 +16,7 @@ class LightGBMCallbackHandler():
             metrics_logger (common.metrics.MetricsLogger)
             node_index (int) : if mpi, provide index of the node
         """
-        self.metrics = {}
+        self.metrics = []
         self.metrics_logger = metrics_logger
         self.logger = logging.getLogger(__name__)
     
@@ -24,7 +25,7 @@ class LightGBMCallbackHandler():
         See https://lightgbm.readthedocs.io/en/latest/_modules/lightgbm/callback.html
         """
         # let's record in the object for future use
-        self.metrics[env.iteration] = env.evaluation_result_list
+        self.metrics.append(env.evaluation_result_list)
 
         # loop on all the evaluation results tuples
         for data_name, eval_name, result, _ in env.evaluation_result_list:
@@ -34,3 +35,20 @@ class LightGBMCallbackHandler():
                 value=result,
                 step=env.iteration # provide iteration as step in mlflow
             )
+
+    def mpi_catch_last_metric(self):
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+
+        all_metrics = {}
+
+        if rank == 0:
+            for i in range(size):
+                received_metric_msg = comm.recv(source=i)
+                all_metrics[i] = received_metric_msg
+        else:
+             comm.send(self.metrics[-1], dest=0)
+        
+        print(all_metrics)
+
