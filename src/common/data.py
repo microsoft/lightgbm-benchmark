3c68a7fddd1775ac5f7dcfb382f72c766de84984
Jeff Omhover <jf.omhover@gmail.com>
2021-09-24 21:47:25 -0700

    remove enum

--- src/common/data.py
+++ src/common/data.py
@@ -1,16 +1,10 @@
 from dataclasses import dataclass
 from omegaconf import MISSING
 from typing import Optional
-from enum import Enum
-
-class LearningTask(Enum):
-    regression = "regression"
-    classification = "classification"
-    ranking = "ranking"
 
 @dataclass
 class synthetic_data_config:
-    task: LearningTask = MISSING
+    task: str = MISSING
     train_samples: int = MISSING
     test_samples: int = MISSING
     inferencing_samples: int = MISSING
