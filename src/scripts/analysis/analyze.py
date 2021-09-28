420d883b4cb3a4a94a4b536153c303255542fe57
Jeff Omhover <jf.omhover@gmail.com>
2021-09-28 16:32:38 -0700

    make it class

--- src/scripts/analysis/analyze.py
+++ src/scripts/analysis/analyze.py
@@ -79,52 +79,67 @@ def get_arg_parser(parser=None):
     return parser
 
 
-def analyze_inferencing(ws, experiment_id, benchmark_id, output_markdown_path):
-    """
-    """
-    print("*** Fetching Experiment")
-    experiment = Experiment(workspace=ws, name=experiment_id)
+class AnalysisEngine():
+    def __init__(self, ws):
+        self.ws = ws
+        self.benchmark_data = []
 
-    print("*** Fetching Benchmark Runs")
-    benchmark_runs = experiment.get_runs(tags={"benchmark_name" : benchmark_id, 'task': 'score'}, include_children=True)
+    def get_benchmark_data(self, experiment_id, **filters):
+        print("*** Fetching Experiment")
+        experiment = Experiment(workspace=self.ws, name=experiment_id)
 
-    # iterate through runs to get all the data we need for the analysis
-    print("*** Iterating through runs")
+        print("*** Fetching Benchmark Runs")
+        benchmark_runs = experiment.get_runs(tags=filters, include_children=True)
 
-    benchmark_data = [] # list of dict for pandas
-    for run in benchmark_runs:
-        benchmark_data_entry = {}
-        run_tags = dict(run.tags)
+        # iterate through runs to get all the data we need for the analysis
+        print("*** Iterating through runs")
 
-        benchmark_data_entry['model_name'] = run_tags.get('benchmark_model', None)
-        if benchmark_data_entry['model_name']:
-            model_pattern = r"synthetic-regr-([0-9]+)cols-model-([0-9]+)trees"
-            model_matched = re.match(model_pattern, benchmark_data_entry['model_name'])
-            if model_matched:
-                benchmark_data_entry['model_trees'] = int(model_matched.group(2))
+        self.benchmark_data = [] # list of dict for pandas
+        for run in benchmark_runs:
+            benchmark_data_entry = {}
+            run_tags = dict(run.tags)
 
-        benchmark_data_entry['dataset_name'] = run_tags.get('benchmark_dataset', None)
-        if benchmark_data_entry['dataset_name']:
-            dataset_pattern = r"synthetic-regr-([0-9]+)cols-inference-([0-9]+)rows"
-            dataset_matched = re.match(dataset_pattern, benchmark_data_entry['dataset_name'])
-            if dataset_matched:
-                benchmark_data_entry['dataset_columns'] = int(dataset_matched.group(1))
-                benchmark_data_entry['dataset_rows'] = int(dataset_matched.group(2))
+            benchmark_data_entry['model_name'] = run_tags.get('benchmark_model', None)
+            if benchmark_data_entry['model_name']:
+                model_pattern = r"synthetic-regr-([0-9]+)cols-model-([0-9]+)trees"
+                model_matched = re.match(model_pattern, benchmark_data_entry['model_name'])
+                if model_matched:
+                    benchmark_data_entry['model_trees'] = int(model_matched.group(2))
 
-        benchmark_data_entry['framework'] = run_tags['framework']
-        benchmark_data_entry['framework_version'] = run_tags.get('framework_version', None)
-        benchmark_data_entry['framework_build'] = run_tags.get('framework_build', None)
+            benchmark_data_entry['dataset_name'] = run_tags.get('benchmark_dataset', None)
+            if benchmark_data_entry['dataset_name']:
+                dataset_pattern = r"synthetic-regr-([0-9]+)cols-inference-([0-9]+)rows"
+                dataset_matched = re.match(dataset_pattern, benchmark_data_entry['dataset_name'])
+                if dataset_matched:
+                    benchmark_data_entry['dataset_columns'] = int(dataset_matched.group(1))
+                    benchmark_data_entry['dataset_rows'] = int(dataset_matched.group(2))
 
-        run_metrics = run.get_metrics()
-        for metric in run_metrics.keys():
-            benchmark_data_entry[metric] = run_metrics[metric]
+            benchmark_data_entry['framework'] = run_tags['framework']
+            benchmark_data_entry['framework_version'] = run_tags.get('framework_version', None)
+            benchmark_data_entry['framework_build'] = run_tags.get('framework_build', None)
 
-        benchmark_data_entry['system_cpu_count'] = run_tags['cpu_count']
-        benchmark_data_entry['system_os'] = run_tags['system']
-        benchmark_data_entry['system_machine'] = run_tags['machine']
+            run_metrics = run.get_metrics()
+            for metric in run_metrics.keys():
+                benchmark_data_entry[metric] = run_metrics[metric]
 
-        print(benchmark_data_entry)
-        benchmark_data.append(benchmark_data_entry)
+            benchmark_data_entry['system_cpu_count'] = run_tags['cpu_count']
+            benchmark_data_entry['system_os'] = run_tags['system']
+            benchmark_data_entry['system_machine'] = run_tags['machine']
+
+            print(benchmark_data_entry)
+            self.benchmark_data.append(benchmark_data_entry)
+
+        return self.benchmark_data
+
+
+    def analyze_inferencing(self, experiment_id, benchmark_id, output_path):
+        # querying runs for specific filters
+        benchmark_data = self.get_benchmark_data(
+            experiment_id=experiment_id,
+            benchmark_name=benchmark_id,
+            task='score'
+        )
+
 
 
 def run(args, unknown_args=[]):
@@ -138,10 +153,13 @@ def run(args, unknown_args=[]):
     logger = logging.getLogger()
 
     # use helper to connect to AzureML
+    print("Connecting to AzureML...")
     ws = azureml_connect_cli(args)
 
+    analysis_engine = AnalysisEngine(ws)
+
     if args.template == 'inferencing':
-        analyze_inferencing(ws, args.experiment_id, args.benchmark_id, args.output)
+        analysis_engine.analyze_inferencing(args.experiment_id, args.benchmark_id, args.output)
     else:
         raise NotImplementedError(f"Analysis template {args.template} does not exist (yet?)")
 
