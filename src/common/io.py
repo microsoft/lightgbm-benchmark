f78022da34cacfa984ce760983f03b8c7db42029
Jeff Omhover <jf.omhover@gmail.com>
2021-10-18 21:46:00 -0700

    reduce data loader to a couple functions

--- src/common/io.py
+++ src/common/io.py
@@ -182,106 +182,33 @@ class DataBatch():
         return self.x[self.batch_idx[idx]], (self.y[self.batch_idx[idx]] if self.y is not None else None)
 
 
-class InputDataLoader():
-    """Utility class to load input data with flexible options from argparse"""
-    # current list of supported loaders
-    SUPPORTED_LOADERS = ['lightgbm', 'numpy', 'libsvm']
+def numpy_data_load(path, delimiter=","):
+    """Loads data using numpy (csv).
+    
+    Args:
+        path (str): path to data file
+    Returns:
+        numpy_array, number_of_rows (int), number of cols (int)
+    """
+    self.logger.info(f"Loading {path} with numpy")
+    # importing at last minute intentionally
+    raw_data = np.loadtxt(path, delimiter=delimiter)
 
-    # prefix used for all argparse
-    DEFAULT_ARG_PREFIX = "input_data"
+    self.logger.info(f"Loaded {path} data has {raw_data.shape[0]} rows and {raw_data.shape[1]} cols")
 
-    def __init__(self,
-                 allowed_loaders=SUPPORTED_LOADERS,
-                 arg_prefix=DEFAULT_ARG_PREFIX,
-                 default_loader=None):
-        """Initialize data loader.
-        Args:
-            allowed_loaders (List[str]): list of supported loaders (can restrict to avoid incompatibilities)
-            arg_prefix (str): which prefix to use for all argparse
-            default_loader (str): name of default loader (if None, will use first in allowed_loaders)
-        """
-        self.allowed_loaders = allowed_loaders
-        self.arg_prefix = arg_prefix
-        self.default_loader = default_loader or allowed_loaders[0]
-        self.logger = logging.getLogger(__name__)
+    return raw_data, raw_data.shape[0], raw_data.shape[1]
 
-    def get_arg_parser(self, parser=None):
-        """Adds arguments for this class
-        Args:
-            parser (argparse.ArgumentParser): an argument parser instance
-        Returns:
-            ArgumentParser: the argument parser instance
-        Notes:
-            if parser is None, creates a new parser instance
-        """
-        # add arguments that are specific to the script
-        if parser is None:
-            parser = argparse.ArgumentParser(__doc__)
+def libsvm_data_load(path):
+    """Loads data using libsvm.
+    
+    Args:
+        path (str): path to data file
+    Returns:
+        (y, x), number_of_rows (int), number of cols (int)
+    """
+    # importing at last minute intentionally
+    from sklearn.datasets import load_svmlight_file
+    
+    x, y = load_svmlight_file(path)
 
-        parser.add_argument(f"--{self.arg_prefix}_loader",
-            required=False, type=str, default=self.default_loader, choices=self.allowed_loaders, help="use numpy for csv, libsvm for libsvm, or lightgbm for both")
-        parser.add_argument(f"--{self.arg_prefix}_batch_size",
-            required=False, type=int, default=0, help="size of batches (default: all data in 1 batch")
-
-        return parser
-
-    def _lightgbm_loader_load(self, path):
-        """Loads data using lightgbm construct().
-        
-        Args:
-            path (str): path to data file
-        Returns:
-            lightgbm_data_reference, number_of_rows (int), number of cols (int)
-        """
-        self.logger.info(f"Loading {path} with lightgbm")
-        # importing at last minute intentionally
-        data = lightgbm_Dataset(path, free_raw_data=False).construct()
-        raw_data = data.get_data()
-
-        self.logger.info(f"Loaded {path} data has {data.num_data()} rows and {data.num_feature()} cols")
-        return raw_data, data.num_data(), data.num_feature()
-
-    def _numpy_loader_load(self, path, batch_size=0):
-        """Loads data using numpy (csv).
-        
-        Args:
-            path (str): path to data file
-        Returns:
-            numpy_array, number_of_rows (int), number of cols (int)
-        """
-        self.logger.info(f"Loading {path} with numpy")
-        # importing at last minute intentionally
-        raw_data = np.loadtxt(path, delimiter=",")
-
-        self.logger.info(f"Loaded {path} data has {raw_data.shape[0]} rows and {raw_data.shape[1]} cols")
-
-        return DataBatch(x=raw_data, y=None, batch_size=batch_size), raw_data.shape[0], raw_data.shape[1]
-
-    def _libsvm_loader_load(self, path, batch_size=0):
-        """Loads data using libsvm.
-        
-        Args:
-            path (str): path to data file
-        Returns:
-            (y, x), number_of_rows (int), number of cols (int)
-        """
-        self.logger.info(f"Loading {path} with libsvm")
-        # importing at last minute intentionally
-        from libsvm.svmutil import svm_read_problem
-
-        y, x = svm_read_problem(path, return_scipy=True)
-
-        self.logger.info(f"Loaded {path}, data (X) has {x.shape[0]} rows and {x.shape[1]} cols")
-        return DataBatch(x=x.toarray(), y=y, batch_size=batch_size), x.shape[0], x.shape[1]
-
-    def load(self, args, path):
-        """Loads data using the right loader"""
-        loader = getattr(args, f"{self.arg_prefix}_loader")
-        batch_size = getattr(args, f"{self.arg_prefix}_batch_size")
-        if loader == "lightgbm":
-            return self._lightgbm_loader_load(path)
-        if loader == "numpy":
-            return self._numpy_loader_load(path, batch_size=batch_size)
-        if loader == "libsvm":
-            return self._libsvm_loader_load(path, batch_size=batch_size)
-        raise NotImplementedError(f"Data loader '{loader}' is not implemented")
+    return (x,y), x.shape[0], x.shape[1]
--- src/scripts/lightgbm_python/score.py
+++ src/scripts/lightgbm_python/score.py
@@ -23,13 +23,7 @@ if COMMON_ROOT not in sys.path:
 
 # useful imports from common
 from common.metrics import MetricsLogger
-from common.io import input_file_path, InputDataLoader
-
-INPUT_DATA_LOADER = InputDataLoader(
-    allowed_loaders = ['numpy', 'libsvm', 'lightgbm'],
-    arg_prefix="data",
-    default_loader="lightgbm"
-)
+from common.io import input_file_path, DataBatch, libsvm_data_load, numpy_data_load
 
 def get_arg_parser(parser=None):
     """Adds component/module arguments to a given argument parser.
@@ -50,13 +44,16 @@ def get_arg_parser(parser=None):
     group_i = parser.add_argument_group("Input Data")
     group_i.add_argument("--data",
         required=True, type=input_file_path, help="Inferencing data location (file path)")
-    INPUT_DATA_LOADER.get_arg_parser(group_i) # add data loading parameters
+    group_i.add_argument(f"--data_loader",
+        required=False, type=str, default="lightgbm", choices=["lightgbm", "libsvm", "numpy"], help="use numpy for csv, libsvm for libsvm, or lightgbm for both")        
     group_i.add_argument("--model",
         required=False, type=input_file_path, help="Exported model location (file path)")
     group_i.add_argument("--output",
         required=False, default=None, type=str, help="Inferencing output location (file path)")
     
     group_params = parser.add_argument_group("Scoring parameters")
+    group_params.add_argument(f"--batch_size",
+        required=False, type=int, default=0, help="size of batches (default: all data in 1 batch")
     group_params.add_argument("--num_threads",
         required=False, default=1, type=int, help="number of threads")
     group_params.add_argument("--predict_disable_shape_check",
@@ -109,7 +106,7 @@ def run(args, unknown_args=[]):
 
     # record relevant parameters
     metrics_logger.log_parameters(
-        batch_size=args.data_batch_size,
+        batch_size=args.batch_size,
         data_loader=args.data_loader,
         num_threads=args.num_threads,
     )
@@ -122,9 +119,24 @@ def run(args, unknown_args=[]):
         os.makedirs(args.output, exist_ok=True)
         args.output = os.path.join(args.output, "predictions.txt")
 
+    if args.batch_size > 0 and args.data_loader == "lightgbm":
+        logger.warning("--data_loader lightgbm does not support --batch_size > 0 (currently)")
+        args.batch_size = 0
+
     logger.info(f"Loading data for inferencing")
     with metrics_logger.log_time_block("time_data_loading"):
-        inference_raw_data_batches, row_count, feature_count = INPUT_DATA_LOADER.load(args, args.data)
+        if args.data_loader == "lightgbm":
+            inference_data = lightgbm.Dataset(args.data, free_raw_data=False).construct()
+            inference_raw_data = inference_data.get_data()
+            row_count = inference_data.num_data()
+            feature_count = inference_data.num_feature()
+        elif args.data_loader == "libsvm":
+            inference_data, row_count, feature_count = libsvm_data_load(args.data)
+            inference_raw_data = inference_data[0] # (x,y) -> x
+        elif args.data_loader == "numpy":
+            inference_data, row_count, feature_count = numpy_data_load(args.data)
+        else:
+            raise NotImplementedError(f"--data_loader {args.data_loader} is not implemented.")
 
     logger.info(f"Loading model from {args.model}")
     booster = lightgbm.Booster(model_file=args.model)
@@ -138,14 +150,22 @@ def run(args, unknown_args=[]):
     logger.info(f"Running .predict()")
     batch_run_times = [] # collect time for each batch
     with metrics_logger.log_time_block("time_inferencing"):
-        for batch_x, _ in inference_raw_data_batches:
-            batch_start_time = time.time()
+        if args.batch_size > 0:
+            inference_batches = DataBatch(x=inference_raw_data, y=None, batch_size=args.batch_size)
+            for data_batch, _ in inference_batches:
+                batch_start_time = time.time()
+                booster.predict(
+                    data=data_batch,
+                    num_threads=args.num_threads,
+                    predict_disable_shape_check=bool(args.predict_disable_shape_check)
+                )
+                batch_run_times.append(time.time() - batch_start_time)
+        else:
             booster.predict(
-                data=batch_x,
+                data=inference_raw_data,
                 num_threads=args.num_threads,
                 predict_disable_shape_check=bool(args.predict_disable_shape_check)
             )
-            batch_run_times.append(time.time() - batch_start_time)
 
     if len(batch_run_times) > 1:
         batch_run_times = numpy.array(batch_run_times)
