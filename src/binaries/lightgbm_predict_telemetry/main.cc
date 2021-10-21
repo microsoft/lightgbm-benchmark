bd37bfb16a6ebb74732a5984ef1245deaef6008c
Jeff Omhover <jf.omhover@gmail.com>
2021-10-20 22:41:53 -0700

    working draft c++ code for lightgbm predict

--- src/binaries/lightgbm_predict_telemetry/main.cc
+++ src/binaries/lightgbm_predict_telemetry/main.cc
@@ -9,16 +9,27 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT license.
 */
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <chrono>
 #include "LightGBM/c_api.h"
 
+using std::cout; 
+using std::cerr;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
 
 // struct to organize arguments for call to LGBM_BoosterPredictForCSRSingleRow()
 // see https://lightgbm.readthedocs.io/en/latest/C-API.html#c.LGBM_BoosterPredictForCSRSingleRow
 struct CSRDataRow_t {
-    int32_t * row_headers;
-    int32_t * indices;
-    float * row;
+    int32_t * row_headers = nullptr;
+    int32_t * indices = nullptr;
+    float * row = nullptr;
     size_t num_features;
     int64_t nindptr;
     int64_t null_elem;
@@ -28,22 +39,90 @@ struct CSRDataRow_t {
 class LibSVMReader {
     private:
         // counts how many rows have been processed so far
-        int row_counter = 0;
-        int max_rows = 10; // TODO: remove this
+        int row_counter;
+        int max_rows; // TODO: remove this
+        int num_features;
+        ifstream * file_handler;
 
     public:
         // Constructor
-        LibSVMReader(const std::string file_path) {
+        LibSVMReader() {
             this->row_counter = 0;
-            this->max_rows = 10;
+            this->max_rows = 100;
+            this->num_features = 0;
+            this->file_handler = nullptr;
         };
         // Destructor
         ~LibSVMReader() {};
+
+        // open the file for parsing
+        int open(const string file_path, int32_t num_features) {
+            this->num_features = num_features;
+            this->file_handler = new ifstream(file_path);
+            string line;
+
+            if (!this->file_handler->is_open()) {
+                cerr << "Could not open the file - '" << file_path << "'" << endl;
+                return EXIT_FAILURE;
+            }
+            return EXIT_SUCCESS;            
+        };
+
+        // close the file handler (duh)
+        void close() {
+            this->file_handler->close();
+        };
+
+        // allocate a new row of data in the given struct
+        CSRDataRow_t * init_row(CSRDataRow_t * row, int32_t num_features) {
+            // if null provide, allocate completely new struct
+            if (row == nullptr) {
+                row = new CSRDataRow_t;
+            }
+
+            // if previous struct provided, free each member + reallocate
+            if (row->row_headers != nullptr) {
+                free(row->row_headers);
+            }
+            row->row_headers = new int32_t[num_features];
+
+            if (row->indices != nullptr) {
+                free(row->indices);
+            }
+            row->indices = new int32_t[num_features];
+
+            if (row->row != nullptr) {
+                free(row->row);
+            }
+            row->row = new float[num_features];
+
+            // just making sure we're not keeping old data
+            for (int i=0; i<num_features; i++) {
+                row->row_headers[i] = -1;
+                row->indices[i] = -1;
+                row->row[i] = 0.0;
+            }
+
+            row->num_features = 0; // tbd during parsing
+
+            // number of rows, that's 1
+            row->nindptr = 1;
+
+            // number of null cells, tbd
+            row->null_elem = 0;
+
+            return row;
+        };
 
         // Iterates on the svm file and returns a row ready to predict
         // returns nullptr when finished
         CSRDataRow_t * iter(CSRDataRow_t * replace_row = nullptr) {
             CSRDataRow_t * csr_row;
+            string input_line;
+
+            if (this->file_handler == nullptr) {
+                throw std::runtime_error("You need to open() the file before iterating on it.");
+            }
 
             // let's fake it for now
             if (this->row_counter >= this->max_rows) {
@@ -52,124 +131,149 @@ class LibSVMReader {
                 this->row_counter++;
             }
 
-            if (replace_row == nullptr) {
-                csr_row = new CSRDataRow_t;
+            // get a line from the file handler
+            if(getline(*this->file_handler, input_line)) {
+                cout << "ROW line=" << this->row_counter;
             } else {
-                csr_row = replace_row;
+                // if we're done
+                return nullptr;
             }
 
-            // let's fake it for now
-            csr_row->row_headers = new int32_t[301];
+            // allocate or re-allocate a new row struct
+            csr_row = this->init_row(replace_row, num_features);
 
-            for (int32_t i=0; i<301; i++) {
-                csr_row->row_headers[i] = i;
+            // split line by whitespace
+            char * token = strtok(const_cast<char*>(input_line.c_str()), " ");
+            // first token is label (float)
+
+            float row_label = atof(token);
+            cout << " label=" << row_label;
+            //csr_row->label = row_label;
+
+            while ((token = strtok(nullptr, " ")) != nullptr)
+            {
+                if (csr_row->num_features > num_features) {
+                    throw std::runtime_error("Number of features found is above expected number.");
+                }
+
+                string token_str = string(token);
+                int colon_index = token_str.find(":");
+                int32_t key = atoi(token_str.substr(0, colon_index).c_str());
+                float value = atof(token_str.substr(colon_index+1,token_str.length()).c_str());
+
+                csr_row->row_headers[csr_row->num_features] = key;
+                csr_row->indices[csr_row->num_features] = key;
+                csr_row->row[csr_row->num_features] = value;
+                csr_row->num_features++;
             }
 
-            csr_row->indices = new int32_t[301];
-            for (int32_t i=0; i<301; i++) {
-                csr_row->indices[i] = i;
-            }
+            csr_row->null_elem = num_features - csr_row->num_features;
+            csr_row->num_features = num_features;
 
-            csr_row->row = new float[301];
-            csr_row->num_features = 301;
-            csr_row->nindptr = 1; // number of rows in sample data
-            csr_row->null_elem = 0;
-
+            cout << " null_elem=" << csr_row->null_elem;
             return csr_row;
         };
 };
 
 // main function obviously
 int main(int argc, char* argv[]) {
+    // ****************
+    // ARGUMENT PARSING
+    // ****************
+
+    if (argc < 3) {
+        cerr << "You need to provide at least 2 arguments:\n\n     lightgbm_predict_telemetry.exe MODELFILEPATH DATAFILEPATH [CUSTOMPARAMS]";
+        return EXIT_FAILURE;
+    }
+    string model_filename = string(argv[1]);
+    string data_filename = string(argv[2]);
+    string custom_params = "";
+
+    // everything after model+data is considered custom params for LightGBM
+    if (argc > 3) {
+        for (int i=3; i<argc; i++) {
+            custom_params += argv[i];
+        }
+    }
+    cout << "LOG running with model=" << model_filename << " data=" << data_filename << " custom_params=" << custom_params << endl;
+
+    // *************
+    // LOADING MODEL
+    // *************
+
+    BoosterHandle model_handle;
     int model_num_trees;
     int num_features;
-    BoosterHandle model_handle;
-    DatasetHandle data_handle;
-    //std::vector<float*> sample_data{sample_data_row};
-
-    // TODO: argument parsing
-    const std::string model_filename = "C:\\Users\\jeomhove\\source\\lightgbm-benchmark\\data\\tests\\fusionmodel.txt";
-    const std::string data_filename = "C:\\Users\\jeomhove\\source\\lightgbm-benchmark\\data\\tests\\data.txt";
-    const std::string custom_params = "";
 
     if (LGBM_BoosterCreateFromModelfile(model_filename.c_str(), &model_num_trees, &model_handle) != 0) {
-        throw std::runtime_error("Could not load LightGBM model from file " + model_filename);
+        throw std::runtime_error("ERROR Could not load LightGBM model from file " + model_filename);
     } else {
-        std::cout << "Successfully loaded LightGBM model from file" + model_filename + "\n";
+        std::cout << "LOG Successfully loaded LightGBM model from file " + model_filename << endl;
     }
 
+    // we need number of features for the libsvm parser
     if (LGBM_BoosterGetNumFeature(model_handle, &num_features) != 0) {
         throw std::runtime_error("Could not get number of features from model");
     } else {
-        std::cout << "num_features=" << num_features << "\n";
+        std::cout << "INPUT num_features=" << num_features << endl;
     }
 
-    if (LGBM_DatasetCreateFromFile(data_filename.c_str(), "", nullptr, &data_handle) != 0) {
-        throw std::runtime_error("Could not load LightGBM data from file " + data_filename);
-    } else {
-        std::cout << "Successfully loaded LightGBM data from file" + data_filename + "\n";
-    }
+    // **********************
+    // LOOP ON DATA + PREDICT
+    // **********************
 
-    // auto dataset = reinterpret_cast<Dataset*>(handle);
-    // void CopySubrow(const Dataset* fullset, const data_size_t* used_indices, data_size_t num_used_indices, bool need_meta_data);
-    // LGBM_BoosterPredictForMats(iterator->second->getHandle(), (const void**)features.second.data(), C_API_DTYPE_FLOAT32, nrows, ncols,
-    //                               C_API_PREDICT_NORMAL, 0, "", &outputLength, outResult.data());
-
-    float * sample_data_mat_row = new float[301];
-    size_t sample_data_mat_num_features = 301;
+    // variables for predictions output
     int64_t out_len = 0;
     double * out_result = new double[1];
 
-    try {
-        if (LGBM_BoosterPredictForMatSingleRow(model_handle, (void*)sample_data_mat_row, C_API_DTYPE_FLOAT32, sample_data_mat_num_features, 1, C_API_PREDICT_NORMAL, 0, 0, custom_params.c_str(), &out_len, out_result) != 0) {
-            std::cout << "failed prediction for some reason";
-        } else {
-            std::cout << "prediction=" << out_result[0] << "\n";
-        }
-    } catch (std::exception e) {
-        std::cout << "exception";
-    }
+    // custom class for reading libsvm
+    LibSVMReader * data_reader = new LibSVMReader();
+    data_reader->open(data_filename, num_features);
 
-    LibSVMReader * data_reader = new LibSVMReader(data_filename);
+    // variables for metrics
+    double prediction_per_request = 0.0;
+    int count_request = 0;
 
+    // loop on each data row
     while (CSRDataRow_t * csr_row = data_reader->iter()) {
-        using std::chrono::high_resolution_clock;
-        using std::chrono::duration_cast;
-        using std::chrono::duration;
-        using std::chrono::milliseconds;
-
+        // NOTE: we add an exception here just in case
         try {
+            // start a clock
             auto t1 = high_resolution_clock::now();
-            if (LGBM_BoosterPredictForCSRSingleRow(model_handle, (void*)csr_row->row_headers, C_API_DTYPE_INT32, csr_row->indices, csr_row->row, C_API_DTYPE_FLOAT32, csr_row->nindptr, csr_row->null_elem, csr_row->num_features, C_API_PREDICT_NORMAL, 0, 0, custom_params.c_str(), &out_len, out_result) != 0) {
-                std::cout << "failed prediction for some reason";
-            } else {
-                std::cout << "prediction=" << out_result[0] << "\n";
-            }
-            auto t2 = high_resolution_clock::now();
-            duration<double, std::milli> ms_double = t2 - t1;
-            std::cout << ms_double.count() << "ms\n";
 
+            // call for LightGBM C API
+            if (LGBM_BoosterPredictForCSRSingleRow(model_handle, (void*)csr_row->row_headers, C_API_DTYPE_INT32, csr_row->indices, csr_row->row, C_API_DTYPE_FLOAT32, csr_row->nindptr, csr_row->null_elem, csr_row->num_features, C_API_PREDICT_NORMAL, 0, 0, custom_params.c_str(), &out_len, out_result) != 0) {
+                std::cout << endl << "ERROR failed prediction for some reason" << endl;
+            } else {
+                std::cout << " prediction=" << out_result[0];
+            }
+
+            // stop the clock
+            auto t2 = high_resolution_clock::now();
+
+            // compute metric
+            duration<double, std::milli> ms_double = t2 - t1;
+            std::cout << " time_ms=" << ms_double.count() << endl;
+
+            // record the rest and iterate
+            prediction_per_request += ms_double.count();
+            count_request++;
         } catch (std::exception e) {
-            std::cout << "exception";
+            std::cout << endl << "ERROR exception" << endl;
         }
     }
 
+    // print out summary metrics
+    cout << "SUMMARY";
+    cout << " prediction_per_request_ms=" << prediction_per_request/count_request << endl;
 
-    //LGBM_BoosterPredictForCSRSingleRow(model_handle, )
-    // LGBM_DatasetGetNumData(DatasetHandle handle, int *out)
-    // LGBM_DatasetGetNumFeature(DatasetHandle handle, int *out)
-    //FastConfigHandle fast_config;
-    //if (LGBM_BoosterPredictForCSRSingleRowFastInit(model_handle, C_API_PREDICT_NORMAL, 0, 0, C_API_DTYPE_FLOAT32, num_features, nullptr, &fast_config) != 0) {
-    //    throw std::runtime_error("Could not create fast config for model");
-    //} else {
-    //    std::cout << "Initialized for FastPredict\n";
-    //}
+    // free resources
+    data_reader->close();
 
-    // LGBM_BoosterPredictForCSRSingleRowFast
     if (model_handle != nullptr) {
-        std::cout << "Free booster\n";
+        std::cout << "LOG Free booster\n";
         LGBM_BoosterFree(model_handle);
     }
 
-    std::cout << "Done.\n";
+    std::cout << "LOG Done.\n";
 }
