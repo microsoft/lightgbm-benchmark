aeec28b0c47be33dcfe7216fd820137002faee60
Jeff Omhover <jf.omhover@gmail.com>
2021-10-27 15:51:24 -0700

    Factor all scripts on a common "components" class (#85)
    * implement components class RunnableScript to use accross components for all routine code
    * try on partition script
    * test on generate
    * shrike compatibility
    * adapt sample script too

--- src/scripts/sample/sample.py
+++ src/scripts/sample/sample.py
@@ -21,150 +21,106 @@ if COMMON_ROOT not in sys.path:
     sys.path.append(str(COMMON_ROOT))
 
 # useful imports from common
-from common.metrics import MetricsLogger
 from common.io import input_file_path
+from common.components import RunnableScript
+
+
+class SampleScript(RunnableScript):
+    def __init__(self):
+        super().__init__(
+            task="sample_task",
+            framework="sample_framework",
+            framework_version="0.0.1"
+        )
+
+    @classmethod
+    def get_arg_parser(cls, parser=None):
+        """Adds component/module arguments to a given argument parser.
+
+        Args:
+            parser (argparse.ArgumentParser): an argument parser instance
+
+        Returns:
+            ArgumentParser: the argument parser instance
+
+        Notes:
+            if parser is None, creates a new parser instance
+        """
+        # add generic arguments
+        parser = RunnableScript.get_arg_parser(parser)
+
+        # add arguments that are specific to the script
+        group_i = parser.add_argument_group("I/O Arguments")
+        group_i.add_argument(
+            "--data",
+            required=True,
+            type=input_file_path,  # use this helper type for a directory containing a single file
+            help="Some input location (directory containing a unique file)",
+        )
+        group_i.add_argument(
+            "--model",
+            required=True,
+            type=input_file_path,  # use this helper type for a directory containing a single file
+            help="Some input location (directory containing a unique file)",
+        )
+        group_i.add_argument(
+            "--output",
+            required=True,
+            default=None,
+            type=str,
+            help="Some output location (directory)",
+        )
+
+        return parser
+
+
+    def run(self, args, logger, metrics_logger, unknown_args):
+        """Run script with arguments (the core of the component)
+
+        Args:
+            args (argparse.namespace): command line arguments provided to script
+            logger (logging.getLogger() for this script)
+            metrics_logger (common.metrics.MetricLogger)
+            unknown_args (list[str]): list of arguments not recognized during argparse
+        """
+        # make sure the output argument exists
+        os.makedirs(args.output, exist_ok=True)
+
+        # and create your own file inside the output
+        args.output = os.path.join(args.output, "predictions.txt")
+
+        # CUSTOM CODE STARTS HERE
+        # below this line is user code
+        logger.info(f"Loading model from {args.model}")
+        booster = lightgbm.Booster(model_file=args.model)
+
+        # to log executing time of a code block, use log_time_block()
+        logger.info(f"Loading data for inferencing")
+        with metrics_logger.log_time_block(metric_name="time_data_loading"):
+            inference_data = lightgbm.Dataset(args.data, free_raw_data=False).construct()
+            inference_raw_data = inference_data.get_data()
+
+        # optional: add data shape as property
+        metrics_logger.set_properties(
+            inference_data_length=inference_data.num_data(),
+            inference_data_width=inference_data.num_feature(),
+        )
+
+        # to log executing time of a code block, use log_time_block()
+        logger.info(f"Running .predict()")
+        with metrics_logger.log_time_block(metric_name="time_inferencing"):
+            booster.predict(data=inference_raw_data)
+
+        # CUSTOM CODE ENDS HERE
 
 
 def get_arg_parser(parser=None):
-    """Adds component/module arguments to a given argument parser.
-
-    Args:
-        parser (argparse.ArgumentParser): an argument parser instance
-
-    Returns:
-        ArgumentParser: the argument parser instance
-
-    Notes:
-        if parser is None, creates a new parser instance
-    """
-    # add arguments that are specific to the script
-    if parser is None:
-        parser = argparse.ArgumentParser(__doc__)
-
-    # recommended: use groups
-    group_i = parser.add_argument_group("I/O Arguments")
-    group_i.add_argument(
-        "--data",
-        required=True,
-        type=input_file_path,  # use this helper type for a directory containing a single file
-        help="Some input location (directory containing a unique file)",
-    )
-    group_i.add_argument(
-        "--model",
-        required=True,
-        type=input_file_path,  # use this helper type for a directory containing a single file
-        help="Some input location (directory containing a unique file)",
-    )
-    group_i.add_argument(
-        "--output",
-        required=True,
-        default=None,
-        type=str,
-        help="Some output location (directory)",
-    )
-
-    group_general = parser.add_argument_group("General parameters")
-    group_general.add_argument(
-        "--verbose",
-        required=False,
-        default=False,
-        type=strtobool,  # use this for bool args, do not use action_store=True
-        help="set True to show DEBUG logs",
-    )
-    group_general.add_argument(
-        "--custom_properties",
-        required=False,
-        default=None,
-        type=str,
-        help="provide custom properties as json dict",
-    )
-
-    return parser
-
-
-def run(args, unknown_args=[]):
-    """Run script with arguments (the core of the component)
-
-    Args:
-        args (argparse.namespace): command line arguments provided to script
-        unknown_args (list[str]): list of arguments not known
-    """
-    # get logger for general outputs
-    logger = logging.getLogger()
-
-    # get Metrics logger for benchmark metrics
-    # below: initialize reporting of metrics with a custom session name
-    metrics_logger = MetricsLogger("sample_framework.sample_task")
-
-    # add common properties to the session
-    metrics_logger.set_properties(
-        task="sample_task", framework="sample_framework", framework_version="0.0.1"
-    )
-
-    # if provided some custom_properties by the outside orchestrator
-    if args.custom_properties:
-        metrics_logger.set_properties_from_json(args.custom_properties)
-
-    # add properties about environment of this script
-    metrics_logger.set_platform_properties()
-
-    # make sure the output argument exists
-    os.makedirs(args.output, exist_ok=True)
-
-    # and create your own file inside the output
-    args.output = os.path.join(args.output, "predictions.txt")
-
-    # CUSTOM CODE STARTS HERE
-    # below this line is user code
-    logger.info(f"Loading model from {args.model}")
-    booster = lightgbm.Booster(model_file=args.model)
-
-    # to log executing time of a code block, use log_time_block()
-    logger.info(f"Loading data for inferencing")
-    with metrics_logger.log_time_block(metric_name="time_data_loading"):
-        inference_data = lightgbm.Dataset(args.data, free_raw_data=False).construct()
-        inference_raw_data = inference_data.get_data()
-
-    # optional: add data shape as property
-    metrics_logger.set_properties(
-        inference_data_length=inference_data.num_data(),
-        inference_data_width=inference_data.num_feature(),
-    )
-
-    # to log executing time of a code block, use log_time_block()
-    logger.info(f"Running .predict()")
-    with metrics_logger.log_time_block(metric_name="time_inferencing"):
-        booster.predict(data=inference_raw_data)
-
-    # CUSTOM CODE ENDS HERE
-
-    # Important: close logging session before exiting
-    metrics_logger.close()
-
+    """ To ensure compatibility with shrike unit tests """
+    return SampleScript.get_arg_parser(parser)
 
 def main(cli_args=None):
-    """Component main function, parses arguments and executes run() function.
-
-    Args:
-        cli_args (List[str], optional): list of args to feed script, useful for debugging. Defaults to None.
-    """
-    # initialize root logger
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    console_handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
-    # construct arg parser and parse arguments
-    parser = get_arg_parser()
-    args, unknown_args = parser.parse_known_args(cli_args)
-
-    logger.setLevel(logging.DEBUG if args.verbose else logging.INFO)
-
-    # run the actual thing
-    run(args, unknown_args)
-
+    """ To ensure compatibility with shrike unit tests """
+    SampleScript.main()
 
 if __name__ == "__main__":
     main()
