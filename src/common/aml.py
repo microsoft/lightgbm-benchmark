d69e583c9a722a80b9765d6638ad6c34211715eb
Jeff Omhover <jf.omhover@gmail.com>
2021-10-26 23:19:34 -0700

    re-introduce old method for back compatibility

--- src/common/aml.py
+++ src/common/aml.py
@@ -2,6 +2,30 @@ import logging
 from azureml.core import Datastore, Dataset
 
 
+def dataset_from_dstore_path(workspace, datastore, datastore_path, validate=True):
+    """ Obtains a local reference for a given datastore and path
+    
+    Args:
+        datastore (str)
+        datastore_path (str)
+
+    Returns:
+        remote_dataset (azureml.core.Dataset)
+    """
+    logger = logging.getLogger(__name__)
+
+    logger.info(f"Connecting to Datastore {datastore}...")
+    datastore = Datastore.get(workspace, datastore)
+
+    logger.info(f"Reading path {datastore_path}...")
+    remote_ds_path = [(datastore, datastore_path)]
+
+    logger.info(f"Registering as dataset...")
+    remote_dataset = Dataset.File.from_files(path=remote_ds_path, validate=validate)
+
+    return remote_dataset
+
+
 def load_dataset_from_data_input_spec(workspace, data_input_spec):
     """ Loads a dataset based on config object data_input_spec (see tasks.py data_input_spec)
     
