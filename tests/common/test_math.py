e1fede7aa6c2337f0e2ed58f02e8b63edfba386f
Jeff Omhover <jf.omhover@gmail.com>
2021-11-24 13:56:21 -0800

    add unit tests

--- tests/common/test_math.py
+++ tests/common/test_math.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from common.math import bootstrap_ci
 
-def test_bootstrap_ci():
+def test_bootstrap_ci_fixed_seed():
     """Testing the bootstrap_ci method, but we can't have a non-deterministic test here. """
     sample_data = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 5.0])
     operators={
@@ -29,6 +29,26 @@ def test_bootstrap_ci():
         operators=operators
     )
 
+    assert returned_values == expected_values
+
+
+def test_bootstrap_ci_no_seed():
+    """Testing the bootstrap_ci method, but we can't have a non-deterministic test here. """
+    sample_data = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 5.0])
+    operators={
+        'mean':np.mean,
+        'p90': (lambda x : np.percentile(x, 90)),
+        'p99': (lambda x : np.percentile(x, 99)),
+    }
+
+    np.random.seed(None) # not const
+
+    returned_values = bootstrap_ci(
+        sample_data,
+        iterations=1000,
+        operators=operators
+    )
+
     for key in operators:
         # check type
         assert key in returned_values
@@ -43,5 +63,3 @@ def test_bootstrap_ci():
         # because it's a bootstrap, these are supposed to be true
         assert min(sample_data) <= ci_left
         assert ci_right <= max(sample_data)
-
-    assert returned_values == expected_values
