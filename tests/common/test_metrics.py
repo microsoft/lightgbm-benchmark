00cc37456ea62223eed1bff4a431ba18a7a96dbf
Jeff Omhover <jf.omhover@gmail.com>
2021-11-29 09:58:28 -0800

    align unit tests

--- tests/common/test_metrics.py
+++ tests/common/test_metrics.py
@@ -222,26 +222,34 @@ def test_log_inferencing_larencies(mlflow_log_metric_mock, mlflow_log_figure_moc
 
     metrics_logger.log_inferencing_latencies(test_latencies, batch_length=test_batch_sizes, factor_to_usecs=1000000.0)
 
+    #assert mlflow_log_metric_mock.call_args_list == []
+
     mlflow_log_metric_mock.assert_has_calls(
         [
             call("prediction_batches", 8, step=None), # len(test_latencies)
             call("prediction_queries", 12, step=None), # sum(test_batch_sizes)
 
             # reference values based on test_latencies above
-            
-            call("prediction_latency_avg", 650000.0, step=None), # sum(test_batch_sizes)
-
-            call("batch_latency_p50_usecs", 450000.0, step=None), # sum(test_batch_sizes)
-            call("batch_latency_p75_usecs", 625000.0, step=None), # sum(test_batch_sizes)
-            call("batch_latency_p90_usecs", 1989999.999999999, step=None), # sum(test_batch_sizes)
-            call("batch_latency_p95_usecs", 3494999.9999999977, step=None), # sum(test_batch_sizes)
-            call("batch_latency_p99_usecs", 4698999.999999998, step=None), # sum(test_batch_sizes)
-
-            call("prediction_latency_p50_usecs", 450000.0, step=None), # sum(test_batch_sizes)
-            call("prediction_latency_p75_usecs", 625000.0, step=None), # sum(test_batch_sizes)
-            call("prediction_latency_p90_usecs", 790000.0, step=None), # sum(test_batch_sizes)
-            call("prediction_latency_p95_usecs", 894999.9999999998, step=None), # sum(test_batch_sizes)
-            call('prediction_latency_p99_usecs', 979000.0, step=None)
+            call('prediction_latency_avg', 650000.0, step=None),
+            call('batch_latency_p50_usecs', 450000.0, step=None),
+            call('batch_latency_p75_usecs', 625000.0, step=None),
+            call('batch_latency_p90_usecs', 1989999.9999999993, step=None),
+            call('batch_latency_p95_usecs', 3494999.9999999977, step=None),
+            call('batch_latency_p99_usecs', 4698999.999999999, step=None),
+            call('prediction_latency_p50_usecs', 450000.0, step=None),
+            call('prediction_latency_p75_usecs', 625000.0, step=None),
+            call('prediction_latency_p90_usecs', 790000.0, step=None),
+            call('prediction_latency_p95_usecs', 894999.9999999999, step=None),
+            call('prediction_latency_p99_usecs', 978999.9999999999, step=None),
         ],
-        any_order=False
+        any_order=True
     )
+
+    # testing logging figures
+    figure_calls = mlflow_log_figure_mock.call_args_list
+    assert mlflow_log_figure_mock.call_count == 2
+    assert len(figure_calls) == 2
+
+    # second argument of each call is file name
+    assert (figure_calls[0].args[1] == "batch_latency_log_histogram.png")
+    assert (figure_calls[1].args[1] == "prediction_latency_log_histogram.png")
