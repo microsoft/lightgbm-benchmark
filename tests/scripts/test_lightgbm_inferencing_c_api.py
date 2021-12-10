ab30b28c415682fd5d163d1b044001c7b7d5ffb7
Jeff Omhover <jf.omhover@gmail.com>
2021-12-10 10:01:05 -0800

    implement unit tests in c api

---
+++ tests/scripts/test_lightgbm_inferencing_c_api.py
@@ -0,0 +1,55 @@
+"""
+Executes the series of scripts end-to-end
+to test LightGBM (python) manual benchmark
+"""
+import os
+import sys
+import tempfile
+import pytest
+from unittest.mock import patch, Mock, call
+
+from scripts.inferencing.lightgbm_c_api import score
+
+# IMPORTANT: see conftest.py for fixtures
+
+@patch('mlflow.log_metric')
+@patch('mlflow.set_tags')
+@patch('scripts.inferencing.lightgbm_c_api.score.subprocess_run')
+@patch('scripts.inferencing.lightgbm_c_api.score.locate_lightgbm_benchmark_binaries')
+def test_lightgbm_c_api_score(locate_binaries_mock, subprocess_run_mock, mlflow_set_tags_mock, mlflow_log_metric_mock, temporary_dir, regression_model_sample, regression_inference_sample):
+    """Tests src/scripts/inferencing/lightgbm_c_api/score.py"""
+    predictions_dir = os.path.join(temporary_dir, "predictions")
+    locate_binaries_mock.return_value = "foo"
+
+    # create a first mock for the return of subprocess
+    subprocess_call_handle_mock = Mock()
+    subprocess_call_handle_mock.returncode = 0
+    subprocess_call_handle_mock.stderr = "# empty logs"
+    subprocess_call_handle_mock.stdout = """
+# fake logs for parsing metrics from C API binaries (cli)
+ROW line=0 label=0.42 null_elem=3 prediction=0.45 time_usecs=45.2
+METRIC foo=342.0
+PROPRETY foo2=bar2
+"""
+    # feed that mock into a subprocess.run() mock
+    subprocess_run_mock.return_value = subprocess_call_handle_mock
+
+    # create test arguments for the script
+    script_args = [
+        "score.py",
+        "--data", regression_inference_sample,
+        "--model", regression_model_sample,
+        "--output", predictions_dir
+    ]
+
+    # replaces sys.argv with test arguments and run main
+    with patch.object(sys, "argv", script_args):
+        score.main()
+
+    # test expected outputs
+    assert os.path.isfile(os.path.join(predictions_dir, "predictions.txt"))
+
+    metric_calls = mlflow_log_metric_mock.call_args_list
+    # 15 = perf metrics + inference metrics + foo
+    assert mlflow_log_metric_mock.call_count == 15
+    assert len(metric_calls) == 15
