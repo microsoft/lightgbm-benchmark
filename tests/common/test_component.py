bbca9e4bd019da04f017fdfe05d70f0bb33612e5
Jeff Omhover <jf.omhover@gmail.com>
2021-12-10 13:07:11 -0800

    add try/except around component run

--- tests/common/test_component.py
+++ tests/common/test_component.py
@@ -21,6 +21,7 @@ class FakeSingleNodeScript(SingleNodeScript):
         with metrics_logger.log_time_block("fake_time_block", step=1):
             time.sleep(1)
 
+
 @patch('mlflow.end_run')
 @patch('mlflow.log_metric')
 @patch('mlflow.set_tags')
@@ -101,3 +102,34 @@ def test_single_node_script_metrics(mlflow_start_run_mock, mlflow_set_tags_mock,
         assert metrics_calls[index+1].args[0] == MetricsLogger._remove_non_allowed_chars(metric_key)
         assert "step" in metrics_calls[index+1].kwargs
         assert metrics_calls[index+1].kwargs["step"] == 0 # using node id as step
+
+
+class FailingSingleNodeScript(SingleNodeScript):
+    def __init__(self):
+        super().__init__(
+            task="failure",
+            framework="pytest",
+            framework_version=pytest.__version__
+        )
+
+    def run(self, args, logger, metrics_logger, unknown_args):
+        # don't do anything
+        with metrics_logger.log_time_block("fake_time_block", step=1):
+            time.sleep(1)
+            raise Exception("Some fake issue occured during code!")
+
+
+@patch('mlflow.end_run')
+@patch('mlflow.log_metric')
+@patch('mlflow.set_tags')
+@patch('mlflow.start_run')
+def test_single_node_script_metrics(mlflow_start_run_mock, mlflow_set_tags_mock, mlflow_log_metric_mock, mlflow_end_run_mock):
+    # just run main
+    with pytest.raises(Exception) as e_test:
+        test_component = FailingSingleNodeScript.main(
+            [
+                "foo.py",
+                "--verbose", "True",
+                "--custom_properties", json.dumps({'benchmark_name':'unittest'})
+            ]
+        )
