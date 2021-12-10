ee22e461025b32a20157388765282d6c55a9d494
Jeff Omhover <jf.omhover@gmail.com>
2021-12-10 13:09:38 -0800

    rollback change in components, put it in a different PR

--- src/common/components.py
+++ src/common/components.py
@@ -171,12 +171,7 @@ class RunnableScript():
         script_instance.initialize_run(args)
 
         # run the actual thing
-        try:
-            script_instance.run(args, script_instance.logger, script_instance.metrics_logger, unknown_args)
-        except BaseException as e:
-            logging.critical(f"Exception occured during run():\n{traceback.format_exc()}")
-            script_instance.finalize_run(args)
-            raise e
+        script_instance.run(args, script_instance.logger, script_instance.metrics_logger, unknown_args)
 
         # close mlflow
         script_instance.finalize_run(args)
