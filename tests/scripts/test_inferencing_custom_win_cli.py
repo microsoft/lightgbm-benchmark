9db467262b8ca6f2a981ad8a8d1d3aadf421f98b
Jeff Omhover <jf.omhover@gmail.com>
2021-12-10 10:20:12 -0800

    impleent custom win cli unit tests

--- tests/scripts/test_inferencing_custom_win_cli.py
+++ tests/scripts/test_inferencing_custom_win_cli.py
@@ -8,31 +8,22 @@ import tempfile
 import pytest
 from unittest.mock import patch, Mock, call
 
-from scripts.inferencing.lightgbm_c_api import score
+from scripts.inferencing.custom_win_cli import score
 
 # IMPORTANT: see conftest.py for fixtures
 
 @patch('mlflow.log_metric')
-@patch('mlflow.set_tags')
-@patch('scripts.inferencing.lightgbm_c_api.score.subprocess_run')
-@patch('scripts.inferencing.lightgbm_c_api.score.locate_lightgbm_benchmark_binaries')
-def test_lightgbm_c_api_score(locate_binaries_mock, subprocess_run_mock, mlflow_set_tags_mock, mlflow_log_metric_mock, temporary_dir, regression_model_sample, regression_inference_sample):
+@patch('scripts.inferencing.custom_win_cli.score.subprocess_run')
+def test_lightgbm_c_api_score(subprocess_run_mock, mlflow_log_metric_mock, temporary_dir, regression_model_sample, regression_inference_sample):
     """Tests src/scripts/inferencing/lightgbm_c_api/score.py"""
     predictions_dir = os.path.join(temporary_dir, "predictions")
-    locate_binaries_mock.return_value = "foo"
 
     # create a first mock for the return of subprocess
     subprocess_call_handle_mock = Mock()
     subprocess_call_handle_mock.returncode = 0
     subprocess_call_handle_mock.stderr = "# empty logs"
-    subprocess_call_handle_mock.stdout = """
-# fake logs for parsing metrics from C API binaries (cli)
-ROW line=0 label=0.42 null_elem=3 prediction=0.45 time_usecs=45.2
-ROW line=1 label=0.42 null_elem=3 prediction=0.45 time_usecs=45.3
-ROW line=3 label=0.42 null_elem=3 prediction=0.45 time_usecs=45.4
-METRIC foo=342.0
-PROPRETY foo2=bar2
-"""
+    subprocess_call_handle_mock.stdout = "# empty logs"
+
     # feed that mock into a subprocess.run() mock
     subprocess_run_mock.return_value = subprocess_call_handle_mock
 
@@ -48,10 +39,10 @@ PROPRETY foo2=bar2
     with patch.object(sys, "argv", script_args):
         score.main()
 
-    # test expected outputs
-    assert os.path.isfile(os.path.join(predictions_dir, "predictions.txt"))
+    # no outputs (yet?)
+    # assert os.path.isfile(os.path.join(predictions_dir, "predictions.txt"))
 
     metric_calls = mlflow_log_metric_mock.call_args_list
-    # 25 = 11 perf metrics + 13 inference metrics + 1 custom (foo)
-    assert mlflow_log_metric_mock.call_count == 25
-    assert len(metric_calls) == 25
+    # 12 = 11 perf metrics + 1 time_inferencing
+    assert mlflow_log_metric_mock.call_count == 12
+    assert len(metric_calls) == 12
