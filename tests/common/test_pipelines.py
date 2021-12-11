3a9169f06776305e1fb7eac78b54ba2a0c9c7f9a
Jeff Omhover <jf.omhover@gmail.com>
2021-12-10 16:41:36 -0800

    align unit tests

--- tests/common/test_pipelines.py
+++ tests/common/test_pipelines.py
@@ -9,29 +9,17 @@ from dataclasses import dataclass
 from omegaconf import DictConfig
 
 from common.pipelines import (
-    aml_connection_config,
-    compute_config,
-    run_config,
-    experiment_config,
-    pipeline_submit_main
+    parse_pipeline_config,
+    azureml_connect,
+    pipeline_submit
 )
 
-@patch('common.pipelines.azureml_connect')
-def test_pipeline_submit_main(aml_connect_mock):
-    # need a mock pipeline function (dsl.pipeline)
-    pipeline_func_mock = Mock()
 
-    # need a mock pipeline instance (returned by pipeline func)
-    pipeline_instance_mock = Mock()
-    pipeline_func_mock.return_value = pipeline_instance_mock
-
-    # no need for a workspace, but we can test call to pipeline instance
-    aml_connect_mock.return_value = "fake_workspace"
-
+def test_parse_pipeline_config():
+    """Creates a config dataclass and tests parsing it from CLI"""
     @dataclass
     class test_config:
         test_param: str = "default_str"
-
 
     script_args = [
         "test_pipelines.py",
@@ -43,32 +31,19 @@ def test_pipeline_submit_main(aml_connect_mock):
         "+compute.linux_cpu=test-cluster",
         "+compute.linux_gpu=test-gpu-cluster",
         "+compute.windows_cpu=test-win-cpu",
-        "test_config.test_param=foo",
+        "test_config.test_param=test_str_value",
         "run.submit=True"
     ]
 
     # replaces sys.argv with test arguments and run main
     with patch.object(sys, "argv", script_args):
-        ret_value = pipeline_submit_main(
-            # config class
-            test_config,
+        pipeline_config = parse_pipeline_config(test_config)
 
-            # dsl.pipeline func
-            pipeline_func_mock,
-
-            # test hardcoded overrides
-            experiment_description="test_description",
-            display_name="test_display_name",
-            tags={'foo':'bar'}
-        )
-
-    # checking call to pipeline_func
-    pipeline_func_mock.assert_called_once()
-
-    # argument should be a DictConfig
-    pipeline_call_args = pipeline_func_mock.call_args
-    pipeline_config = pipeline_call_args.args[0]
+    # test return value type
     assert isinstance(pipeline_config, DictConfig)
+
+    # test some custom value
+    assert pipeline_config.test_config.test_param == "test_str_value"
 
     # checking config fields (see dataclass above)  
     # aml connect
@@ -82,8 +57,26 @@ def test_pipeline_submit_main(aml_connect_mock):
     assert pipeline_config.compute.linux_gpu == "test-gpu-cluster"
     assert pipeline_config.compute.windows_cpu == "test-win-cpu"
 
-    # custom params
-    assert pipeline_config.test_config.test_param == "foo"
+    return pipeline_config
+
+def test_pipeline_submit():
+    # need a mock pipeline mock (Pipeline)
+    pipeline_instance_mock = Mock()
+    workspace_mock = "fake_workspace"
+
+    # reusing config from previous test
+    pipeline_config = test_parse_pipeline_config()
+
+    pipeline_run = pipeline_submit(
+        workspace_mock,
+        pipeline_config,
+        pipeline_instance_mock,
+
+        # test hardcoded overrides
+        experiment_description="test_description",
+        display_name="test_display_name",
+        tags={'foo':'bar'}
+    )
 
     pipeline_instance_mock.validate.assert_called_once()
     pipeline_instance_mock.validate.assert_called_with(
