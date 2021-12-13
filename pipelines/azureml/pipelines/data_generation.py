da2795da2ef58a87f3ff1b93f6e93ca35e800416
Jeff Omhover <jf.omhover@gmail.com>
2021-12-13 15:41:53 -0800

    Rewrite data generation pipeline without Shrike (#171)
    * try sdk native for data generation (wip)
    * restore config management (wip)
    * pipeline validation working (wip)
    * create cli function
    * remove manifest
    * add submit and revise run/experiments
    * fix call to pipeline in github build
    * write some unit test for main function

--- pipelines/azureml/pipelines/data_generation.py
+++ pipelines/azureml/pipelines/data_generation.py
@@ -1,195 +1,161 @@
 """
-Runs LightGBM using distributed (mpi) training.
+Generates synthetic data with multiple parameters.
+
+See config file /pipelines/azureml/conf/experiments/data-generation.yaml
 
 to execute:
-> python pipelines/data_generation.py --config-dir ./conf --config-name experiments/data-generation run.submit=True
+> python pipelines/azureml/pipelines/data_generation.py --exp-config pipelines/azureml/conf/experiments/data-generation.yaml +run.submit=True
 """
 # pylint: disable=no-member
 # NOTE: because it raises 'dict' has no 'outputs' member in dsl.pipeline construction
 import os
 import sys
 import json
+import logging
+import argparse
+
+# config management
 from dataclasses import dataclass
-from omegaconf import MISSING, OmegaConf
+from omegaconf import OmegaConf, MISSING
 from typing import Optional, List
+
+# AzureML
+from azure.ml.component import Component
 from azure.ml.component import dsl
-from shrike.pipeline.pipeline_helper import AMLPipelineHelper
-from azure.ml.component.environment import Docker
 
 # when running this script directly, needed to import common
-LIGHTGBM_BENCHMARK_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src'))
+LIGHTGBM_REPO_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+SCRIPTS_SOURCES_ROOT = os.path.join(LIGHTGBM_REPO_ROOT, 'src')
 
-if LIGHTGBM_BENCHMARK_ROOT not in sys.path:
-    print(f"Adding {LIGHTGBM_BENCHMARK_ROOT} to path")
-    sys.path.append(str(LIGHTGBM_BENCHMARK_ROOT))
+if SCRIPTS_SOURCES_ROOT not in sys.path:
+    logging.info(f"Adding {SCRIPTS_SOURCES_ROOT} to path")
+    sys.path.append(str(SCRIPTS_SOURCES_ROOT))
 
-from common.sweep import SweepParameterParser
 from common.tasks import data_generation_task
+from common.pipelines import (
+    parse_pipeline_config,
+    azureml_connect,
+    pipeline_submit,
+    COMPONENTS_ROOT
+)
 
-class DataGenerationPipeline(AMLPipelineHelper):
-    """Runnable/reusable pipeline helper class
+### CONFIG DATACLASS ###
 
-    This class inherits from AMLPipelineHelper which provides
-    helper functions to create reusable production pipelines for SmartCompose.
+# Step 1 : to configure your pipeline, add all your fields inside a
+# properly defined dataclass, pipeline_cli_main will figure out how
+# to read that config from a given yaml file + hydra override commands
+
+@dataclass
+class data_generation_config: # pylint: disable=invalid-name
+    """ Config object constructed as a dataclass.
+
+    The name of this class will be used as namespace in your config yaml file.
     """
-    @classmethod
-    def get_config_class(cls):
-        """ Returns the config object (dataclass) for this runnable script.
+    # NOTE: all those values are REQUIRED in your yaml config file
+    benchmark_name: str = MISSING
 
-        Returns:
-            dataclass: class for configuring this runnable pipeline.
-        """
-        @dataclass
-        class data_generation: # pylint: disable=invalid-name
-            """ Config object constructed as a dataclass.
+    tasks: List[data_generation_task] = MISSING
 
-            NOTE: the name of this class will be used as namespace in your config yaml file.
-            See conf/reference/evaluate_qas_model.yaml for an example.
-            """
-            # NOTE: all those values are REQUIRED in your yaml config file
-            benchmark_name: str = MISSING
+    # OUTPUT REGISTRATION
+    register_outputs: bool = False
+    register_outputs_prefix: str = "synthetic"
 
-            tasks: List[data_generation_task] = MISSING
+### PIPELINE COMPONENTS ###
 
-            # OUTPUT REGISTRATION
-            register_outputs: bool = False
-            register_outputs_prefix: str = "synthetic"
+# Step 2 : your pipeline consists in assembling components
+# load those components from local yaml specifications
+# use COMPONENTS_ROOT as base folder
 
-        # return the dataclass itself
-        # for helper class to construct config file
-        return data_generation
+generate_data_component = Component.from_yaml(yaml_file=os.path.join(COMPONENTS_ROOT, "data_processing", "generate_data", "spec.yaml"))
 
+### DATA GENERATION PIPELINE ###
 
-    def build(self, config):
-        """ Builds a pipeline function for this pipeline using AzureML SDK (dsl.pipeline).
+# Step 3: your pipeline consists in creating a python function
+# decorated with @dsl.pipeline.
+# You can create as many subgraphs as you want,
+# but `pipeline_cli_main` will need one pipeline function
+# taking a single config argument, not a pipeline parameter.
 
-        This method should build your graph using the provided config object.
-        Your pipeline config will be under config.CONFIGNAME.*
-        where CONFIGNAME is the name of the dataclass returned by get_config_class()
+@dsl.pipeline(
+    name="generate_all_datasets", # pythonic name
+    non_pipeline_parameters=["config"] # required to use config object
+)
+def data_generation_main_pipeline_function(config):
+    """Pipeline's main building function.
 
-        This method returns a constructed pipeline function (decorated with @dsl.pipeline).
+    Args:
+        config (DictObject): the pipeline configuration object containing pipeline config dataclass
+            This is not an actual pipeline parameter
+    
+    Returns:
+        None
+    """
+    benchmark_custom_properties = json.dumps({
+        'benchmark_name' : config.data_generation_config.benchmark_name
+    })
 
-        Args:
-            config (DictConfig): configuration object (see get_config_class())
+    for generation_task in config.data_generation_config.tasks:
+        generate_data_step = generate_data_component(
+            learning_task = generation_task.task,
+            train_samples = generation_task.train_samples,
+            test_samples = generation_task.test_samples,
+            inferencing_samples = generation_task.inferencing_samples,
+            n_features = generation_task.n_features,
+            n_informative = generation_task.n_informative,
+            random_state = 5,
+            verbose = False,
+            custom_properties = benchmark_custom_properties
+        )
+        generate_data_step.runsettings.configure(target=config.compute.linux_cpu)
 
-        Returns:
-            dsl.pipeline: the function to create your pipeline
-        """
-        # Data modules
-        generate_data_module = self.module_load("generate_synthetic_data")
-
-        pipeline_name = f"data_generation"
-        pipeline_description = f"Data Generation for the LightGBM benchmark"
-
-        # Here you should create an instance of a pipeline function (using your custom config dataclass)
-        @dsl.pipeline(name=pipeline_name, # pythonic name
-                      description=pipeline_description,
-                      default_datastore=config.compute.noncompliant_datastore)
-        def data_generation_pipeline_function(task,
-                                              train_samples=10000,
-                                              test_samples=1000,
-                                              inferencing_samples=10000,
-                                              n_features=4000,
-                                              n_informative=4000,
-                                              benchmark_custom_properties=None):
-            """Pipeline function for this graph.
-
-            Args:
-                TODO
-
-            Returns:
-                dict[str->PipelineOutputData]: a dictionary of your pipeline outputs
-                    for instance to be consumed by other graphs
-            """          
-            generate_data_step = generate_data_module(
-                learning_task = task,
-                train_samples = train_samples,
-                test_samples = test_samples,
-                inferencing_samples = inferencing_samples,
-                n_features = n_features,
-                n_informative = n_informative,
-                random_state = 5,
-                verbose = False,
-                custom_properties = benchmark_custom_properties
+        if config.data_generation_config.register_outputs:
+            dataset_prefix = "{prefix}-{task}-{cols}cols".format(
+                prefix=config.data_generation_config.register_outputs_prefix,
+                task=generation_task.task,
+                cols=generation_task.n_features
             )
-            self.apply_smart_runsettings(generate_data_step)
-
-            # return {key: output}'
-            return {
-                'train' : generate_data_step.outputs.output_train,
-                'test' : generate_data_step.outputs.output_test,
-                'inference' : generate_data_step.outputs.output_inference
-            }
-
-        # finally return the function itself to be built by helper code
-        return data_generation_pipeline_function
-
-
-    def pipeline_instance(self, pipeline_function, config):
-        """ Given a pipeline function, creates a runnable instance based on provided config.
-
-        This is used only when calling this as a runnable pipeline using .main() function (see below).
-        The goal of this function is to map the config to the pipeline_function inputs and params.
-
-        Args:
-            pipeline_function (function): the pipeline function obtained from self.build()
-            config (DictConfig): configuration object (see get_config_class())
-
-        Returns:
-            azureml.core.Pipeline: the instance constructed with its inputs and params.
-        """
-        benchmark_custom_properties = json.dumps({
-            'benchmark_name' : config.data_generation.benchmark_name
-        })
-        full_pipeline_description="\n".join([
-            "Generate all datasets for lightgbm benchmark",
-            "```yaml""",
-            OmegaConf.to_yaml(config),
-            "```"
-        ])
-
-        # Here you should create an instance of a pipeline function (using your custom config dataclass)
-        @dsl.pipeline(name="generate_all_datasets", # pythonic name
-                      description=full_pipeline_description,
-                      default_datastore=config.compute.noncompliant_datastore)
-        def generate_all_tasks():
-            for generation_task in config.data_generation.tasks:
-                generation_task_subgraph_step = pipeline_function(
-                    task=generation_task.task,
-                    train_samples=generation_task.train_samples,
-                    test_samples=generation_task.test_samples,
-                    inferencing_samples=generation_task.inferencing_samples,
-                    n_features=generation_task.n_features,
-                    n_informative=generation_task.n_informative,
-                    benchmark_custom_properties=benchmark_custom_properties
-                )
-
-                if config.data_generation.register_outputs:
-                    dataset_prefix = "{prefix}-{task}-{cols}cols".format(
-                        prefix=config.data_generation.register_outputs_prefix,
-                        task=generation_task.task,
-                        cols=generation_task.n_features
-                    )
-                    
-                    generation_task_subgraph_step.outputs.train.register_as(
-                        name=f"{dataset_prefix}-{generation_task.train_samples}samples-train",
-                        create_new_version=True
-                    )  
-                    generation_task_subgraph_step.outputs.test.register_as(
-                        name=f"{dataset_prefix}-{generation_task.test_samples}samples-test",
-                        create_new_version=True
-                    )  
-                    generation_task_subgraph_step.outputs.inference.register_as(
-                        name=f"{dataset_prefix}-{generation_task.inferencing_samples}samples-inference",
-                        create_new_version=True
-                    )  
+            
+            generate_data_step.outputs.train.register_as(
+                name=f"{dataset_prefix}-{generation_task.train_samples}samples-train",
+                create_new_version=True
+            )  
+            generate_data_step.outputs.test.register_as(
+                name=f"{dataset_prefix}-{generation_task.test_samples}samples-test",
+                create_new_version=True
+            )  
+            generate_data_step.outputs.inference.register_as(
+                name=f"{dataset_prefix}-{generation_task.inferencing_samples}samples-inference",
+                create_new_version=True
+            )  
 
 
-        # and we return that function so that helper can run it.
-        return generate_all_tasks()
+### MAIN BLOCK ###
 
+# Step 4: implement main block using helper functions
 
-# NOTE: main block is necessary only if script is intended to be run from command line
 if __name__ == "__main__":
-    # calling the helper .main() function
-    DataGenerationPipeline.main()
+    # use parse helper function to get arguments from CLI
+    config = parse_pipeline_config(data_generation_config)
+    
+    # you'll need a workspace object to connect
+    workspace = azureml_connect(config)
+
+    # run the pipeline function with the given arguments
+    pipeline_instance = data_generation_main_pipeline_function(config)
+
+    # generate a nice markdown description
+    experiment_description="\n".join([
+        "Generating synthetic datasets (see yaml below).",
+        "```yaml",
+        "data_generation_config:",
+        OmegaConf.to_yaml(config.data_generation_config),
+        "```"
+    ])
+
+    # validate/submit the pipeline (if run.submit=True)
+    pipeline_submit(
+        workspace,
+        config,
+        pipeline_instance,
+        experiment_description=experiment_description
+    )
