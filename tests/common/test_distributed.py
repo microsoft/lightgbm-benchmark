be771b95881a52aee04e400a28c58df0bb77863a
Jeff Omhover <jf.omhover@gmail.com>
2021-12-14 11:59:04 -0800

    add test on failure

--- tests/common/test_distributed.py
+++ tests/common/test_distributed.py
@@ -30,7 +30,7 @@ class FakeMultiNodeScript(MultiNodeScript):
 @patch('mlflow.set_tags')
 @patch('mlflow.start_run')
 @patch('common.distributed.MPIHandler')
-def test_multi_node_script_metrics(mpi_handler_mock, mlflow_start_run_mock, mlflow_set_tags_mock, mlflow_log_metric_mock, mlflow_end_run_mock):
+def test_multi_node_script(mpi_handler_mock, mlflow_start_run_mock, mlflow_set_tags_mock, mlflow_log_metric_mock, mlflow_end_run_mock):
     # fake mpi initialization + config
     mpi_handler_mock().mpi_config.return_value = mpi_config_class(
         1, # world_size
@@ -63,3 +63,38 @@ def test_multi_node_script_metrics(mpi_handler_mock, mlflow_start_run_mock, mlfl
         [{'key':'fake_time_block', 'step':1}], # user_metrics
         mlflow_log_metric_mock
     )
+
+
+class FailingMultiNodeScript(MultiNodeScript):
+    def __init__(self):
+        super().__init__(
+            task="failure",
+            framework="pytest",
+            framework_version=pytest.__version__
+        )
+
+    def run(self, args, logger, metrics_logger, unknown_args):
+        # don't do anything
+        with metrics_logger.log_time_block("fake_time_block", step=1):
+            time.sleep(1)
+            raise Exception("Some fake issue occured during code!")
+
+@patch('common.distributed.MPIHandler')
+def test_multi_node_script_failure(mpi_handler_mock):
+    # fake mpi initialization + config
+    mpi_handler_mock().mpi_config.return_value = mpi_config_class(
+        1, # world_size
+        0, # world_rank
+        False, # mpi_available
+        True, # main_node
+    )
+
+    # just run main
+    with pytest.raises(Exception) as e_test:
+        test_component = FailingMultiNodeScript.main(
+            [
+                "foo.py",
+                "--verbose", "True",
+                "--custom_properties", json.dumps({'benchmark_name':'unittest'})
+            ]
+        )
