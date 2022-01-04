d1b9ab676ecf6f6a968e9af214b7c1dd7f18384d
Jeff Omhover <jf.omhover@gmail.com>
2022-01-04 13:13:05 -0800

    create random regression data generator (batch)

---
+++ src/common/data.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# derived from https://github.com/scikit-learn/scikit-learn/blob/7e1e6d09bcc2eaeba98f7e737aac2ac782f0e5f1/sklearn/datasets/_samples_generator.py#L506
+# for generating data in batches
+class RegressionDataGenerator():
+    def __init__(self,
+                 n_features: int,
+                 n_informative: int,
+                 n_targets: int,
+                 bias: float,
+                 noise: float,
+                 seed: int):
+        self.n_features = n_features
+        self.n_informative = min(n_features, n_informative)
+        self.n_targets = n_targets
+        self.bias = bias
+        self.noise = noise
+        self.generator = np.random.RandomState(seed)
+
+        # Generate a ground truth model with only n_informative features being non-zeros
+        self.ground_truth = np.zeros((self.n_features, self.n_targets))
+        self.ground_truth[:n_informative, :] = 100 * self.generator.rand(self.n_informative, self.n_targets)
+
+    def generate_batch(self, batch_size):
+        # Randomly generate a well conditioned input set
+        X = self.generator.randn(batch_size, self.n_features)
+
+        y = np.dot(X, self.ground_truth) + self.bias
+
+        # Add noise
+        if self.noise > 0.0:
+            y += self.generator.normal(scale=self.noise, size=y.shape)
+
+        y = np.squeeze(y)
+
+        return X, y
---
+++ tests/common/test_data.py
@@ -0,0 +1,71 @@
+"""Tests src/common/data.py"""
+import os
+import pytest
+from unittest.mock import call, Mock, patch
+
+from common.data import RegressionDataGenerator
+
+def test_regression_data_generator():
+    """Tests format of outputs of RegressionDataGenerator"""
+    generator = RegressionDataGenerator(
+        n_features=100,
+        n_informative=50,
+        n_targets=1,
+        bias=1.0,
+        noise=1.0,
+        seed=4
+    )
+
+    for i in range(10):
+        batch = generator.generate_batch(64)
+
+        assert batch is not None
+        assert isinstance(batch, tuple)
+        assert len(batch) == 2
+
+        X, y = batch
+        assert X is not None
+        assert y is not None
+
+        assert X.shape == (64, 100)
+        assert y.shape == (64,)
+
+def test_regression_data_generator_reproducibility():
+    """Tests initializing generator with seeds"""
+    generator1 = RegressionDataGenerator(
+        n_features=100,
+        n_informative=50,
+        n_targets=1,
+        bias=1.0,
+        noise=1.0,
+        seed=4
+    )
+    X1,y1 = generator1.generate_batch(64)
+
+    generator2 = RegressionDataGenerator(
+        n_features=100,
+        n_informative=50,
+        n_targets=1,
+        bias=1.0,
+        noise=1.0,
+        seed=5
+    )
+    X2,y2 = generator2.generate_batch(64)
+
+    generator3 = RegressionDataGenerator(
+        n_features=100,
+        n_informative=50,
+        n_targets=1,
+        bias=1.0,
+        noise=1.0,
+        seed=4 # <<< Equal to generator 1
+    )
+    X3,y3 = generator3.generate_batch(64)
+
+    # if using same seed twice, should be equal strictly
+    assert (X1 == X3).all()
+    assert (y1 == y3).all()
+
+    # if using different seeds, likely to be different
+    assert (X1 != X2).all()
+    assert (y1 != y2).all()
