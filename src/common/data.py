48c407411be43eaeb9d8a997d59c9834d9787778
Jeff Omhover <jf.omhover@gmail.com>
2022-01-05 17:06:16 -0800

    commenting

--- src/common/data.py
+++ src/common/data.py
@@ -1,8 +1,16 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+Helper code to handle/process data
+"""
+
 import numpy as np
 
 # derived from https://github.com/scikit-learn/scikit-learn/blob/7e1e6d09bcc2eaeba98f7e737aac2ac782f0e5f1/sklearn/datasets/_samples_generator.py#L506
 # for generating data in batches
 class RegressionDataGenerator():
+    """Generator for regression data"""
     def __init__(self,
                  batch_size: int,
                  n_features: int,
@@ -11,6 +19,7 @@ class RegressionDataGenerator():
                  bias: float,
                  noise: float,
                  seed: int):
+        """Initializes the generator"""
         self.batch_size = batch_size
         self.n_features = n_features
         self.n_informative = min(n_features, n_informative)
@@ -24,6 +33,12 @@ class RegressionDataGenerator():
         self.ground_truth[:n_informative, :] = 100 * self.generator.rand(self.n_informative, self.n_targets)
 
     def generate(self):
+        """Generate one batch of data.
+
+        Returns:
+            X (numpy.ndarray)
+            y (numpy.ndarray)
+        """
         # Randomly generate a well conditioned input set
         X = self.generator.randn(self.batch_size, self.n_features)
 
