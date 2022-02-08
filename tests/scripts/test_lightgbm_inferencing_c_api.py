d623c67617268d256e475e75385aa0d1263a9371
Jeff Omhover <jf.omhover@gmail.com>
2022-02-07 16:03:36 -0800

    increment metric numbers

--- tests/scripts/test_lightgbm_inferencing_c_api.py
+++ tests/scripts/test_lightgbm_inferencing_c_api.py
@@ -56,6 +56,6 @@ PROPRETY foo2=bar2
     assert os.path.isfile(os.path.join(predictions_dir, "predictions.txt"))
 
     metric_calls = mlflow_log_metric_mock.call_args_list
-    # 25 = 11 perf metrics + 13 inference metrics + 1 custom (foo)
-    assert mlflow_log_metric_mock.call_count == 25
-    assert len(metric_calls) == 25
+    # 32 = 18 perf metrics + 13 inference metrics + 1 custom (foo)
+    assert mlflow_log_metric_mock.call_count == 32
+    assert len(metric_calls) == 23
