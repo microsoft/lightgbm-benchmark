64dc98771a72870d5c0257c88f0c89bd4834779a
Jeff Omhover <jf.omhover@gmail.com>
2022-02-09 15:07:52 -0800

    fix ray init in unit tests

--- tests/scripts/test_lightgbm_inferencing_ray.py
+++ tests/scripts/test_lightgbm_inferencing_ray.py
@@ -8,11 +8,22 @@ import pytest
 from unittest.mock import patch
 
 from scripts.inferencing.lightgbm_ray import score
-
+from common.distributed import multinode_config_class
 # IMPORTANT: see conftest.py for fixtures
 
-def test_lightgbm_ray_score(temporary_dir, regression_model_sample, regression_inference_sample):
+@patch('ray.shutdown')
+@patch('ray.init')
+@patch('common.distributed.MultiNodeMPIDriver')
+def test_lightgbm_ray_score(mpi_driver_mock, ray_init_mock, ray_shutdown_mock, ray_init_fixture, temporary_dir, regression_model_sample, regression_inference_sample):
     """Tests src/scripts/inferencing/lightgbm_ray/score.py"""
+    # fake mpi initialization + config
+    mpi_driver_mock().get_multinode_config.return_value = multinode_config_class(
+        1, # world_size
+        0, # world_rank
+        False, # mpi_available
+        True, # main_node
+    )
+
     predictions_dir = os.path.join(temporary_dir, "predictions")
 
     # create test arguments for the script
